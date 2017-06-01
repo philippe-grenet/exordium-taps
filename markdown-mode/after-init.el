8f92ee511ad9cdbcae8c44f25937880af13f1e33
pgrenet <pgrenet@bloomberg.net>
2017-06-01 10:28:07 -0400

    Add markdown

---
+++ markdown-mode/after-init.el
@@ -0,0 +1,14 @@
+;;;; Local extensions to Exordium: Markdown mode
+
+(require 'markdown-mode)
+(setq markdown-command "/opt/local/bin/pandoc")
+(add-hook 'markdown-mode-hook 'flyspell-mode)
+
+(defun straighten-quotes (beg end)
+  "Replace 'smart quotes' in buffer or region with ascii quotes."
+  (interactive "r")
+  (format-replace-strings '(("\x201C" . "\"")
+                            ("\x201D" . "\"")
+                            ("\x2018" . "'")
+                            ("\x2019" . "'"))
+                          nil beg end))
