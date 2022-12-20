fff9d364e5f4decf5978712ae3c9685f9288c700
Philippe Grenet <pgrenet@bloomberg.net>
2022-12-20 17:11:34 -0500

    Treemacs settings

--- java/after-init.el
+++ java/after-init.el
@@ -1,15 +1,31 @@
 ;; See https://github.com/emacs-lsp/lsp-java
 
 (require 'helm)
-(setq helm-split-window-default-side 'below)
-(setq helm-split-window-in-side-p t)
+(setq helm-split-window-default-side 'below
+      helm-split-window-inside-p t)
+
+(require 'treemacs)
+(setq treemacs-wrap-around nil
+      treemacs-width 65
+      treemacs-text-scale -0.3
+      treemacs-is-never-other-window t)
+(treemacs-filewatch-mode 0)  ; to many files descriptors otherwise
 
 (require 'lsp-java)
 (add-hook 'java-mode-hook #'lsp)
 
 (use-package lsp-ui)
-(use-package lsp-mode :hook ((lsp-mode . lsp-enable-which-key-integration)))
+(use-package lsp-mode
+  :hook ((lsp-mode . lsp-enable-which-key-integration)))
 (use-package helm-lsp)
-(use-package lsp-treemacs)
+
+(use-package lsp-treemacs
+  :after lsp)
+
+(use-package dap-mode
+  :ensure t
+  :after lsp-mode
+  :config
+  (dap-auto-configure-mode))
 
 ;; Keys: see https://emacs-lsp.github.io/lsp-mode/page/keybindings/
